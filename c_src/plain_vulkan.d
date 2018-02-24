1b0d5246a9f38c40518cc67c93908b41a62f00da
Maksim Krzhemenevskiy <mkrzhemenevsky@mkrzhemenevsky.vvt.local>
2018-02-24 16:29:00 +0700

    update .gitignore

--- c_src/plain_vulkan.d
+++
@@ -1,5 +0,0 @@
-c_src/plain_vulkan.o: c_src/plain_vulkan.c \
- /home/regikul/.kerl/installs/erl20.2/erts-9.2/include/erl_nif.h \
- /home/regikul/.kerl/installs/erl20.2/erts-9.2/include/erl_drv_nif.h \
- /home/regikul/.kerl/installs/erl20.2/erts-9.2/include/erl_int_sizes_config.h \
- /home/regikul/.kerl/installs/erl20.2/erts-9.2/include/erl_nif_api_funcs.h
