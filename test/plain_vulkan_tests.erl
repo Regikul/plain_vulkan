3be785dc3f2fec2b65abfe9f1ec86423c1249d2f
Maksim Krzhemenevskiy <regikul@gmail.com>
2018-04-22 16:46:09 +0700

    update flow_test

--- test/plain_vulkan_tests.erl
+++ test/plain_vulkan_tests.erl
@@ -1,15 +1,31 @@
 -module(plain_vulkan_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+-include("../include/plain_vulkan.hrl").
 
 flow_test() ->
   {ok, Instance} = plain_vulkan:create_instance(?MODULE_STRING),
-  case plain_vulkan:count_physical_devices(Instance) of
-    {ok, _CountDev} when is_integer(_CountDev) andalso _CountDev > 0 -> ok;
-    out_of_device_memory -> ok;
-    out_of_host_memory -> ok;
-    init_failed -> ok
-  end,
-  {ok, Devices} = plain_vulkan:enumerate_physical_devices(Instance),
-  _FirstDeviceProperties = plain_vulkan:get_physical_device_properties(hd(Devices)),
+  ok = case plain_vulkan:count_physical_devices(Instance) of
+         {ok, _CountDev} when is_integer(_CountDev) andalso _CountDev > 0 -> ok;
+         Else -> Else
+       end,
+  {ok, [PhysDevice | _ ]} = plain_vulkan:enumerate_physical_devices(Instance),
+  _DeviceProperties = plain_vulkan:get_physical_device_properties(PhysDevice),
+  {ok, QueueFamilyProperties} = plain_vulkan:get_physical_device_queue_family_properties(PhysDevice),
+  ComputeInfo = #vk_device_queue_create_info{} = lists:foldl(fun find_compute_queue/2, 0, QueueFamilyProperties),
+  DeviceCreateInfo = #vk_device_create_info{queue_create_infos = [ComputeInfo]
+                                            ,enabled_features = #vk_physical_device_features{}
+                                           },
+  {ok, Device} = plain_vulkan:create_device(PhysDevice, DeviceCreateInfo),
+
+  plain_vulkan:destroy_device(Device),
+
   ok = plain_vulkan:destroy_instance(Instance).
+
+find_compute_queue(#vk_queue_family_properties{queueFlags = Flags}, Count) when is_integer(Count) ->
+  case lists:member(compute, Flags) of
+    'true' -> #vk_device_queue_create_info{queue_count = 1, queue_family_index = Count, queue_priorities = [0.0]};
+    'false' -> Count + 1
+  end;
+find_compute_queue(_, #vk_device_queue_create_info{} = Info) ->
+  Info.
